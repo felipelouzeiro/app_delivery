983d21cf5fbd815db57c449042351a0e967d717f
Felipe Dias <felipeferreiradias1@gmail.com>
2022-02-22 19:11:11 -0300

    feat: add login schema

--- back-end/src/utils/joiSchemas.js
+++ back-end/src/utils/joiSchemas.js
@@ -1,10 +1,10 @@
-// const Joi = require('joi');
+const Joi = require('joi');
 
-// const loginSchema = Joi.object({
-//   registry: Joi.string().required(),
-//   password: Joi.string().required(),
-// });
+const loginSchema = Joi.object({
+  registry: Joi.string().required().empty(),
+  password: Joi.string().required().empty(),
+});
 
-// module.exports = {
-//   loginSchema
-// }
+module.exports = {
+  loginSchema,
+};
