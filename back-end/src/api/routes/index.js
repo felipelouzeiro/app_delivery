383eac72a4afa807d8dd3a0e82b4289c2aaf9950
HenriqueCursino <henriquecursino46@gmail.com>
2022-02-22 18:47:30 -0300

    feat: add register route

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const registerRouter = require('./register');
 
 const router = express.Router();
 
 // Ex: router.use('/users', usersRouter);
+router.use('/register', registerRouter);
 
 module.exports = router;
