52f3b9d8da9c7a44bf12b9dbe5d4f16da63f4e62
HenriqueCursino <henriquecursino46@gmail.com>
2022-02-23 16:40:29 -0300

    refactor: change token generation

--- back-end/src/services/register.js
+++ back-end/src/services/register.js
@@ -1,5 +1,5 @@
 const md5 = require('md5');
-const { User } = require('../database/models');
+const { user } = require('../database/models');
 const { badRequest, conflict } = require('../utils/dictionary');
 const errorConstructor = require('../utils/errorConstructor');
 const { registerSchema } = require('../utils/joiSchemas');
@@ -8,13 +8,14 @@ const registerUser = async (name, email, password, role) => {
   const { error } = registerSchema.validate({ name, email, password, role });
   if (error) throw errorConstructor(badRequest, error.message);
 
-  const emailExist = await User.findOne({ where: { email } });
+  const emailExist = await user.findOne({ where: { email } });
   if (emailExist) throw errorConstructor(conflict, 'Email already exist');
 
   const hash = md5(password);
-  const { dataValues } = await User.create({ name, email, password: hash, role });
-  
-  return dataValues;
+  const { dataValues } = await user.create({ name, email, password: hash, role });
+  const token = generateToken(dataValues);
+
+  return token;
 }
 
 module.exports = {
