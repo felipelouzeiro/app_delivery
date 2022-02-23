df7e706de8bb3eac3127bb247d2e3f5bd46f9d54
felipe_louzeiro <fl.louzeiro@gmail.com>
2022-02-23 16:35:01 -0300

    Feat: add middleware authentication

---
+++ back-end/src/middlewares/authentication.js
@@ -0,0 +1,25 @@
+const { verifyToken } = require('../utils/auth');
+const { unauthorized } = require('../utils/dictionary');
+const errorConstructor = require('../utils/errorConstructor');
+
+const authentication = async (req, _res, next) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) throw errorConstructor(unauthorized, 'missing auth token')
+    
+    const user = await verifyToken(authorization);
+
+    if (!user)throw errorConstructor(unauthorized, 'expired token')
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  authentication,
+};
