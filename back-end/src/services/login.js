d6460207b08570a9bd46aeca32ffed11b6152f98
HenriqueCursino <henriquecursino46@gmail.com>
2022-02-23 16:41:50 -0300

    fix: fixed password logic and model name

--- back-end/src/services/login.js
+++ back-end/src/services/login.js
@@ -1,21 +1,23 @@
 const errorConstructor = require('../utils/errorConstructor');
 const { loginSchema } = require('../utils/joiSchemas');
 const { unauthorized } = require('../utils/dictionary');
-const { User } = require('../database/models');
+const { user } = require('../database/models');
 const { generateToken } = require('../utils/auth');
+const md5 = require('md5')
 
 const loginService = async (email, password) => {
   const { error } = loginSchema.validate({ email, password });
 
   if (error) throw errorConstructor(unauthorized, error.message);
 
-  const user = await User.findOne({ where: { email } });
+  const { dataValues } = await user.findOne({ where: { email } });
+  const encryptedPassword = md5(password);
 
-  if (!user || user.password !== password) {
+  if (!dataValues || dataValues.password !== encryptedPassword) {
     throw errorConstructor(unauthorized, 'Incorrect email or password');
   }
 
-  const { password: _password, ...userWithoutPassword } = user;
+  const { password: _password, ...userWithoutPassword } = dataValues;
 
   const token = generateToken(userWithoutPassword);
 
