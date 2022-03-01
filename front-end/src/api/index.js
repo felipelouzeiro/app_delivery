cc4b6511d504f1fc1fc67e8c5adc74f148002ed2
felipe_louzeiro <fl.louzeiro@gmail.com>
2022-03-01 17:07:03 -0300

    Feat: adds post function to register users in the database

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -1,9 +1,29 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'http://localhost:3001';
 
 // export const getProducts = async () => {
 //   const products = await axios.get(`${BASE_URL}/product`);
 
 //   return products;
 // };
+
+export const register = async (data) => {
+  const dataRegister = {
+    name: data.name,
+    email: data.email,
+    password: data.password,
+    role: 'customer',
+  };
+
+  try {
+    const response = await axios.post(`${BASE_URL}/register`, dataRegister);
+    return response;
+  } catch (error) {
+    return false;
+  }
+};
+
+export default {
+  register,
+};
