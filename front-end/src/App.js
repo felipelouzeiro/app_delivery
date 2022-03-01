6eaac6dc05ea2ce7e553220df3616e93e2d95bc2
felipe_louzeiro <fl.louzeiro@gmail.com>
2022-03-01 16:58:20 -0300

    Feat: add register route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Switch>
-      <Route />
+      <Route path="/register">
+        <Register />
+      </Route>
     </Switch>
   );
 }
