7eef0c7274a0bf96a52bf6654aecc1d06d550d40
felipe_louzeiro <fl.louzeiro@gmail.com>
2022-03-01 17:04:06 -0300

    Feat: add registration form

---
+++ front-end/src/components/RegisterForm.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { register } from '../api';
+import { registerSchema } from '../utils/schemas';
+
+export default function RegisterForm() {
+  const [formDisable, setFormDisable] = useState(true);
+  const [errorDisabled, setErrorDisabled] = useState(false);
+
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    registerSchema.isValid({ name, email, password })
+      .then((valid) => {
+        if (valid) setFormDisable(false);
+        else setFormDisable(true);
+      });
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await register({ name, email, password });
+    if (!response) setErrorDisabled(true);
+    else {
+      const { data: { token } } = response;
+      localStorage.setItem('token', JSON.stringify(token));
+    }
+  };
+
+  return (
+    <section>
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="nome">
+          Nome
+          <input
+            onChange={ ({ target }) => setName(target.value) }
+            value={ name }
+            placeholder="Seu nome"
+            id="nome"
+            type="text"
+            data-testid="common_register__input-name"
+          />
+        </label>
+
+        <label htmlFor="email">
+          Email
+          <input
+            onChange={ ({ target }) => setEmail(target.value) }
+            value={ email }
+            placeholder="seu-email@site.com.br"
+            id="email"
+            type="text"
+          />
+        </label>
+
+        <label htmlFor="password">
+          Senha
+          <input
+            onChange={ ({ target }) => setPassword(target.value) }
+            value={ password }
+            placeholder="**********"
+            id="password"
+            type="password"
+            data-testid="common_register__input-password"
+          />
+        </label>
+
+        <button
+          data-testid="common_register__button-register"
+          type="submit"
+          disabled={ formDisable }
+        >
+          CADASTRAR
+        </button>
+      </form>
+      {
+        errorDisabled && (
+          <p data-testid="common_register__element-invalid_register">
+            Email já registrado!
+          </p>
+        )
+      }
+    </section>
+  );
+}
