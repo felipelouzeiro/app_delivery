f440d003b02c6ba422c666bf490aedd831c2b24f
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-03 16:55:03 -0300

    Feat: add reducer on store

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { counterSlice } from './slices/counterSlice(EXAMPLE)';
+import productSlice from './slices/productSlice';
 
-export default store = configureStore({
+export default configureStore({
   reducer: {
-    counter: counterSlice,
+    chosenProduct: productSlice,
   },
 });
