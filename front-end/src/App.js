d86c6666809c1c65dc2ac4329d9d850f89f5603d
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-03 16:47:33 -0300

    Feat: add route customer/products

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Product from './pages/products';
 
 function App() {
   return (
     <Switch>
-      <Route />
+      <Route exact path="/customer/products" component={ Product } />
     </Switch>
   );
 }
