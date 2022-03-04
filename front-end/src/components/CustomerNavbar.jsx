b36e5b58a204bf9577dc24cb8e0601f43f2d642e
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-04 12:40:54 -0300

    Feat: create function to get localStorage Info and logout app

--- front-end/src/components/CustomerNavbar.jsx
+++ front-end/src/components/CustomerNavbar.jsx
@@ -1,35 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export function CustomerNavbar() {
+  const [userData, setUserData] = useState({});
+  const history = useHistory();
+
+  const logOut = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    setUserData(user);
+  }, []);
+
   return (
-    <div className="customer-navbar-container">
-      <div className="requests-container">
-        <p
-          data-testid="customer_products__element-navbar-link-products"
-        >
-          Produtos
-        </p>
-        <p
-          data-testid="customer_products__element-navbar-link-orders"
-        >
-          Meus Pedidos
-        </p>
-      </div>
-      <div className="customer-name-container">
-        <p
-          data-testid="customer_products__element-navbar-user-full-name"
-        >
-          Nome do Usuário
-        </p>
-      </div>
-      <div className="customer-exit-container">
-        <p
-          data-testid="customer_products__element-navbar-link-logout"
-        >
-          Sair
-        </p>
-      </div>
-    </div>
+    <nav>
+      { !userData.name ? <h1>Loading</h1> : (
+        <div className="customer-navbar-container">
+          <p
+            data-testid="customer_products__element-navbar-link-products"
+          >
+            Produtos
+          </p>
+          <p
+            data-testid="customer_products__element-navbar-link-orders"
+          >
+            Meus Pedidos
+          </p>
+          <div className="customer-name-container">
+            <p
+              data-testid="customer_products__element-navbar-user-full-name"
+            >
+              { userData.name }
+            </p>
+          </div>
+          <div className="customer-exit-container">
+            <button
+              type="button"
+              data-testid="customer_products__element-navbar-link-logout"
+              onClick={ logOut }
+            >
+              Sair
+            </button>
+          </div>
+        </div>
+      ) }
+    </nav>
   );
 }
 
