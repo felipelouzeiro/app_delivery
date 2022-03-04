f9ad50682978c3af988f3a75c7c98b2f8649a164
HenriqueCursino <henriquecursino46@gmail.com>
2022-03-04 16:04:13 -0300

    fix: coment myRequest

--- front-end/src/pages/MyRequests.jsx
+++ front-end/src/pages/MyRequests.jsx
@@ -1,11 +1,12 @@
-import React, { useCallback, useState } from 'react';
-import getAllOrders from '../api';
-import cardMyRequests from '../components/CardMyRequests';
+// import React, { useCallback, useState } from 'react';
+// import getAllOrders from '../api';
+// import cardMyRequests from '../components/CardMyRequests';
 
-export default function MyRequests () {
-  const { setOrdersCustomer, ordersCustomer } = useState();
+// export default function MyRequests () {
+//   const [ setOrdersCustomer, ordersCustomer ] = useState();
 
-  const getRequests = useCallback(async () => {
-    
-  })
-}
+//   const getRequests = useCallback(async () => {
+//     const allOrders = await getAllOrders();
+//     await setOrdersCustomer(allOrders);
+//   })
+// }
