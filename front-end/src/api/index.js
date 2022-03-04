cd886980230bb11e897a87c88ee5651817f6779b
HenriqueCursino <henriquecursino46@gmail.com>
2022-03-04 15:38:28 -0300

    feat: add fetch getAllOrders

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -1,9 +1,21 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'http://localhost:3001';
 
-// export const getProducts = async () => {
-//   const products = await axios.get(`${BASE_URL}/product`);
+export const getAllOrders = async (Authorization) => {
+  const { role, token } = Authorization;
+  const URL_ORDER = `${BASE_URL}/${role}/orders`;
 
-//   return products;
-// };
+  try {
+    const { data } = await axios.get(URL_ORDER, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default getAllOrders;
