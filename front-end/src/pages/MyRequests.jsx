32c4ecf0b756d0bac8c45629266d91e75148b301
HenriqueCursino <henriquecursino46@gmail.com>
2022-03-04 17:50:59 -0300

    feat: implements MyResquests.jsx

--- front-end/src/pages/MyRequests.jsx
+++ front-end/src/pages/MyRequests.jsx
@@ -1,12 +1,36 @@
-// import React, { useCallback, useState } from 'react';
-// import getAllOrders from '../api';
-// import cardMyRequests from '../components/CardMyRequests';
+import React, { useCallback, useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import getAllOrders from '../api';
+import NavBar from '../components/CustomerNavbar';
+import CardMyRequests from '../components/CardMyRequests';
 
-// export default function MyRequests () {
-//   const [ setOrdersCustomer, ordersCustomer ] = useState();
+export default function MyRequests () {
+  const [ ordersCustomer, setOrdersCustomer ] = useState();
 
-//   const getRequests = useCallback(async () => {
-//     const allOrders = await getAllOrders();
-//     await setOrdersCustomer(allOrders);
-//   })
-// }
+  const socket = io('http://localhost:3001');
+
+  const getRequests = useCallback(async () => {
+    const allOrders = await getAllOrders();
+    await setOrdersCustomer(allOrders);
+  })
+
+  useEffect(() => getRequests(), [getRequests, setOrdersCustomer]);
+
+  socket.on('', async () => getRequests());
+
+  socket.on('', async () => getRequests());
+
+  return (
+    <div>
+      <NavBar />
+      <div>
+        {ordersCustomer.map((order) => (
+          <CardMyRequests
+            key={ order.id }
+            orders={ order }
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
