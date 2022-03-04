3d1beb0c2e047610da96a2e2347b90e9a0e9c112
HenriqueCursino <henriquecursino46@gmail.com>
2022-03-04 15:33:33 -0300

    fix: export store

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from './slices/counterSlice(EXAMPLE)';
 
-export default store = configureStore({
+export default configureStore({
   reducer: {
     counter: counterSlice,
   },
