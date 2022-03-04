603ed43adb9bdb8ed0e481825948ae99b64aba3d
HenriqueCursino <henriquecursino46@gmail.com>
2022-03-04 18:55:24 -0300

    refact: change socket path

--- front-end/src/pages/MyRequests.jsx
+++ front-end/src/pages/MyRequests.jsx
@@ -1,18 +1,16 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { io } from 'socket.io-client';
-import getAllOrders from '../api';
+import socket from '../utils/socketClient';
+import { getAllOrders } from '../api';
 import NavBar from '../components/CustomerNavbar';
 import CardMyRequests from '../components/CardMyRequests';
 
-export default function MyRequests () {
-  const [ ordersCustomer, setOrdersCustomer ] = useState();
-
-  const socket = io('http://localhost:3001');
+export default function MyRequests() {
+  const [ordersCustomer, setOrdersCustomer] = useState();
 
   const getRequests = useCallback(async () => {
     const allOrders = await getAllOrders();
     await setOrdersCustomer(allOrders);
-  })
+  });
 
   useEffect(() => getRequests(), [getRequests, setOrdersCustomer]);
 
@@ -32,5 +30,5 @@ export default function MyRequests () {
         ))}
       </div>
     </div>
-  )
+  );
 }
