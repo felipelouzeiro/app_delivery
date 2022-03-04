d979f67f2fab1a9ac720592bfb42c753a8895c7c
Cassio Pereira <cassiorodp@gmail.com>
2022-03-04 19:29:51 -0300

    feat: add socket logic

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const app = require('express')();
 const cors = require('cors');
 const path = require('path');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET'],
+  },
+});
 const errorHandler = require('../middlewares/errorHandler');
 const router = require('./routes');
-
-const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -12,4 +18,4 @@ app.use(router);
 app.use('/images', express.static(path.resolve(__dirname, '../../public/images')));
 app.use(errorHandler);
 
-module.exports = app;
+module.exports = http;
