12a13fffaa42be71510a2ab4ceb212c57182e607
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-04 22:24:22 -0300

    Fix: Clean component and mark place on page Checkout

--- front-end/src/components/CheckoutAddress.jsx
+++ front-end/src/components/CheckoutAddress.jsx
@@ -3,15 +3,7 @@ import React from 'react';
 function CheckoutAddress() {
   return (
     <div className="product-page">
-      <h1>Checkout Address</h1>
-      <tr>
-        <th>Item</th>
-        <th>Descrição</th>
-        <th>Quantidade</th>
-        <th>Valor Unitário</th>
-        <th>Sub-total</th>
-        <th>Remover Item</th>
-      </tr>
+      <h2> Address Form Here!!! </h2>
     </div>
   );
 }
