ef4946523885f32fe47ba98a3681226b0dc6c914
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-06 12:25:35 -0300

    Feat: build CheckoutAddress component

--- front-end/src/components/CheckoutAddress.jsx
+++ front-end/src/components/CheckoutAddress.jsx
@@ -1,9 +1,115 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { getUsers, postSales } from '../api';
 
 function CheckoutAddress() {
+  const chosenProduct = useSelector((state) => state.chosenProduct.chosenProducts);
+  const [filterSellers, setFilterSellers] = useState([]);
+  const [address, setAdress] = useState('');
+  const [number, setNumber] = useState('');
+  const [seller, setSeller] = useState('Fulana Pereira');
+  const history = useHistory();
+
+  const getSellerId = (personSeller) => {
+    const sellerId = filterSellers.find((userSeller) => userSeller.name === personSeller);
+    return sellerId.id;
+  };
+
+  const saleProducts = () => {
+    const products = [];
+    chosenProduct
+      .forEach((prod) => {
+        const { id, quantity } = prod;
+        const product = { productId: id, quantity };
+        products.push(product);
+      });
+    return products;
+  };
+
+  const sumTotal = () => {
+    let totalPrice = 0;
+    chosenProduct
+      .forEach((prod) => {
+        totalPrice += prod.quantity * prod.price;
+      });
+    return totalPrice;
+  };
+
+  const finishOrder = async (e) => {
+    e.preventDefault();
+    const totalPrice = sumTotal();
+    const products = saleProducts();
+    const sellerId = getSellerId(seller);
+
+    const salesData = {
+      sellerId,
+      totalPrice,
+      deliveryAddress: address,
+      deliveryNumber: number,
+      products,
+    };
+    console.log(salesData);
+    const { data } = await postSales(salesData);
+    console.log(data);
+    history.push(`/orders/${data.id}`);
+  };
+
+  useEffect(() => {
+    getUsers()
+      .then((response) => setFilterSellers(response.data
+        .filter((client) => client.role === 'seller')));
+  }, []);
+
   return (
     <div className="product-page">
-      <h2> Address Form Here!!! </h2>
+      { console.log(seller) }
+      <label htmlFor="sellers">
+        P. Vendedora Responsável
+        <select
+          required
+          name="sellers"
+          data-testid="customer_checkout__select-seller"
+          onChange={ (event) => setSeller(event.target.value) }
+        >
+          {
+            filterSellers
+              .map((userSeller, index) => (
+                <option
+                  key={ index }
+                >
+                  { userSeller.name }
+                </option>))
+          }
+        </select>
+      </label>
+      <label htmlFor="address">
+        Endereço
+        <input
+          required
+          type="text"
+          name="address"
+          onChange={ (event) => setAdress(event.target.value) }
+          data-testid="customer_checkout__input-address"
+        />
+      </label>
+      <label htmlFor="number">
+        Número
+        <input
+          required
+          type="text"
+          name="number"
+          onChange={ (event) => setNumber(event.target.value) }
+          data-testid="customer_checkout__input-addressNumber"
+        />
+      </label>
+      <button
+        type="submit"
+        onClick={ finishOrder }
+        data-testid="customer_checkout__button-submit-order"
+      >
+        Finalizar Pedido
+      </button>
     </div>
   );
 }
