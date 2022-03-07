2b7a177d397738c026922679e7c9587e027b89f7
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-07 17:58:58 -0300

    Feat: create sellers route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import Product from './pages/Products';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Checkout from './pages/Checkout';
+import SellerOrders from './pages/SellerOrders';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       </Route>
       <Route exact path="/customer/products" component={ Product } />
       <Route exact path="/customer/checkout" component={ Checkout } />
+      <Route exact path="/seller/orders" component={ SellerOrders } />
     </Switch>
   );
 }
