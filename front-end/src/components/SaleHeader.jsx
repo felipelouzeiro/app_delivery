8a7d3a2cdd8215e2b2eedfe1a23f444c53165962
felipe_louzeiro <fl.louzeiro@gmail.com>
2022-03-08 14:53:22 -0300

    Feat: add SaleHeader component

--- front-end/src/components/SaleHeader.jsx
+++ front-end/src/components/SaleHeader.jsx
@@ -4,9 +4,11 @@ import { getUsers } from '../api';
 import formDate from '../utils/dateFormat';
 
 function SaleHeader({ order }) {
+  console.log('🚀 ~ file: SaleHeader.jsx ~ line 7 ~ SaleHeader ~ order', order);
   const { status, id, sellerId, saleDate } = order[0];
   const [sellerName, setSellerName] = useState('');
   const [deliveryCheck, setDeliveryCheck] = useState(true);
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
     getUsers()
@@ -20,31 +22,93 @@ function SaleHeader({ order }) {
     if (status === 'Em Trânsito') setDeliveryCheck(false);
   }, [status]);
 
-  const orderdate = formDate(saleDate);
-  const dataTestidBase = 'customer_order_details__element-order-details-label';
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    setUserData(user);
+  }, []);
 
-  return (
+  const orderdate = formDate(saleDate);
+  const dataTestidCustomerBase = 'customer_order_details__element-order-details-label';
+  const dataTestidSellerBase = 'seller_order_details__element-order-details-label';
+
+  const useHeader = () => (
     <div className="details-heading">
-      <p data-testid={ `${dataTestidBase}-order-id` }>
-        {id}
-      </p>
-      <p data-testid={ `${dataTestidBase}-seller-name` }>
-        {sellerName}
-      </p>
-      <p data-testid={ `${dataTestidBase}-order-date` }>
-        {orderdate}
-      </p>
-      <p data-testid={ `${dataTestidBase}-delivery-status` }>
-        {status}
-      </p>
-      <button
-        type="button"
-        data-testid="customer_order_details__button-delivery-check"
-        disabled={ deliveryCheck }
-      >
-        Marcar como entregue
-      </button>
+      {userData.role === 'customer' && (
+        <>
+          <p data-testid={ `${dataTestidCustomerBase}-order-id` }>
+            {id}
+          </p>
+          <p data-testid={ `${dataTestidCustomerBase}-seller-name` }>
+            {sellerName}
+          </p>
+          <p data-testid={ `${dataTestidCustomerBase}-order-date` }>
+            {orderdate}
+          </p>
+          <p data-testid={ `${dataTestidCustomerBase}-delivery-status` }>
+            {status}
+          </p>
+          <button
+            type="button"
+            data-testid="customer_order_details__button-delivery-check"
+            disabled={ deliveryCheck }
+          >
+            Marcar como entregue
+          </button>
+        </>
+      )}
+      {userData.role === 'seller' && (
+        <>
+          <p data-testid={ `${dataTestidSellerBase}-order-id` }>
+            {id}
+          </p>
+          <p data-testid={ `${dataTestidSellerBase}-order-date` }>
+            {orderdate}
+          </p>
+          <p data-testid={ `${dataTestidSellerBase}-delivery-status` }>
+            {status}
+          </p>
+          <button
+            type="button"
+            data-testid="seller_order_details__button-preparing-check"
+            // disabled={  }
+          >
+            Preparar pedido
+          </button>
+          <button
+            type="button"
+            data-testid="seller_order_details__button-dispatch-check"
+            // disabled={  }
+          >
+            Saiu para entrega
+          </button>
+        </>
+      )}
     </div>
+  );
+  // FALTA LÓGICA DOS BOTÕES
+  return (
+    useHeader()
+    // <div className="details-heading">
+    //   <p data-testid={ `${dataTestidCustomerBase}-order-id` }>
+    //     {id}
+    //   </p>
+    //   <p data-testid={ `${dataTestidCustomerBase}-seller-name` }>
+    //     {sellerName}
+    //   </p>
+    //   <p data-testid={ `${dataTestidCustomerBase}-order-date` }>
+    //     {orderdate}
+    //   </p>
+    //   <p data-testid={ `${dataTestidCustomerBase}-delivery-status` }>
+    //     {status}
+    //   </p>
+    //   <button
+    //     type="button"
+    //     data-testid="customer_order_details__button-delivery-check"
+    //     disabled={ deliveryCheck }
+    //   >
+    //     Marcar como entregue
+    //   </button>
+    // </div>
   );
 }
 
