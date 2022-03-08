7001fec4d492c66c810d79b15396e8bca454d05c
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-08 10:11:25 -0300

    Refactor: Change CustomerNavbar to generic component

--- front-end/src/components/CustomerNavbar.jsx
+++ front-end/src/components/CustomerNavbar.jsx
@@ -15,10 +15,10 @@ export function CustomerNavbar() {
     setUserData(user);
   }, []);
 
-  return (
-    <nav>
-      { !userData.name ? <h1>Loading</h1> : (
-        <div className="customer-navbar-container">
+  const navbarCustomerSeller = () => (
+    <div className="customer-navbar-container">
+      { userData.role === 'customer' && (
+        <>
           <Link to="/customer/products">
             <p
               data-testid="customer_products__element-navbar-link-products"
@@ -33,24 +33,49 @@ export function CustomerNavbar() {
               Meus Pedidos
             </p>
           </Link>
-          <div className="customer-name-container">
-            <p
-              data-testid="customer_products__element-navbar-user-full-name"
-            >
-              { userData.name }
-            </p>
-          </div>
-          <div className="customer-exit-container">
-            <button
-              type="button"
-              data-testid="customer_products__element-navbar-link-logout"
-              onClick={ logOut }
-            >
-              Sair
-            </button>
-          </div>
-        </div>
+        </>
       ) }
+      { userData.role === 'seller' && (
+        <Link to="/seller/orders">
+          <p
+            data-testid="customer_products__element-navbar-link-orders"
+          >
+            Meus Pedidos
+          </p>
+        </Link>
+      ) }
+    </div>);
+
+  return (
+    <nav>
+      { navbarCustomerSeller()}
+      { userData.role === 'administrator' && (
+        <Link to="/admin/manage">
+          <p
+            data-testid="customer_products__element-navbar-link-products"
+          >
+            Gerenciar Usuários
+          </p>
+        </Link>
+      )}
+      <div className="customer-navbar-container">
+        <div className="customer-name-container">
+          <p
+            data-testid="customer_products__element-navbar-user-full-name"
+          >
+            { userData.name }
+          </p>
+        </div>
+        <div className="customer-exit-container">
+          <button
+            type="button"
+            data-testid="customer_products__element-navbar-link-logout"
+            onClick={ logOut }
+          >
+            Sair
+          </button>
+        </div>
+      </div>
     </nav>
   );
 }
