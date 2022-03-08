b37a6afdfc32c513e435252af51eaa41513a3711
Cassio Pereira <cassiorodp@gmail.com>
2022-03-08 09:43:57 -0300

    feat: add Redirect Rendering

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Form from '../components/Form';
 import beerGif from '../images/beer.gif';
 
 export default function Login() {
+  const user = JSON.parse(localStorage.getItem('user'));
   return (
-    <main>
-      <img src={ beerGif } alt="beer gif" width="100px" />
-      <h1>José Delivery</h1>
-      <Form />
-    </main>
+    <>
+      {user && user.role === 'customer' && <Redirect to="/customer/products" />}
+      <main>
+        <img src={ beerGif } alt="beer gif" width="100px" />
+        <h1>José Delivery</h1>
+        <Form />
+      </main>
+    </>
   );
 }
