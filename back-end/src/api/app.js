10b9e18ba731cfee040282b9140a61f25fd8a139
Cassio Pereira <cassiorodp@gmail.com>
2022-03-08 18:04:58 -0300

    feat: add socket.io

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,12 +3,12 @@ const app = require('express')();
 const cors = require('cors');
 const path = require('path');
 const http = require('http').createServer(app);
-// const io = require('socket.io')(http, {
-//   cors: {
-//     origin: 'http://localhost:3000',
-//     methods: ['GET'],
-//   },
-// });
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET'],
+  },
+});
 const errorHandler = require('../middlewares/errorHandler');
 const router = require('./routes');
 
@@ -17,5 +17,6 @@ app.use(cors());
 app.use(router);
 app.use('/images', express.static(path.resolve(__dirname, '../../public/images')));
 app.use(errorHandler);
+require('../sockets/status')(io);
 
 module.exports = http;
