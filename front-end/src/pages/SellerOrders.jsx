a26ef438e64df5268be5e5daa630ec0126445499
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-08 10:14:26 -0300

    Feat: Create components CardSellerSales and SellerOrders

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import socket from '../utils/socketClient';
+import { getAllOrders } from '../api';
+import NavBar from '../components/CustomerNavbar';
+import CardSellerSales from '../components/CardSellerSales';
 
-export default function SellerOrders() {
+export default function MyRequests() {
+  const [sellerSales, setSellerSales] = useState([]);
+
+  useEffect(() => {
+    getAllOrders()
+      .then(({ sales }) => setSellerSales(sales));
+  }, []);
+
   return (
     <div>
-      <h1>Seller Orders !!!</h1>
+      <NavBar />
+      {sellerSales.length && (
+        sellerSales.map((order) => (
+          <CardSellerSales
+            key={ order.id }
+            orders={ order }
+          />
+        ))
+      )}
     </div>
   );
 }
