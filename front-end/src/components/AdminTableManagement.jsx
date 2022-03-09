f542bc7efca8f928115a522e7d80c8fe56e2e8f7
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-09 15:40:45 -0300

    Feat: create component AdminTableManagement

--- front-end/src/components/AdminTableManagement.jsx
+++ front-end/src/components/AdminTableManagement.jsx
@@ -1,53 +1,74 @@
-// import React, { useEffect, useState } from 'react';
-// import { getUsers } from '../api/index';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeUser } from '../redux/slices/userSlice';
+import { deleteUser } from '../api';
 
-// function AdminTableManagement() {
-//   const [allUsers, setAllUsers] = useState([]);
+function AdminTableManagement() {
+  const users = useSelector((state) => state.users.users);
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     const users = getUsers();
-//     setAllUsers(users);
-//   });
+  const renderRole = (role) => {
+    if (role === 'customer') return 'Cliente';
+    if (role === 'seller') return 'P. Vendedora';
+    return 'Admin';
+  };
 
-//   return (
-//     <>
-//     { allUsers.map(({ id, name, email, role }, index) => (
-//       <div>
-//         <ul>
-//           <li key={ index }>
-//             <h3
-//               data-testid={ `admin_manage__element-user-table-item-number-${id}` }
-//             >
-//               { index + 1 }
-//             </h3>
-//             <h3
-//               data-testid={ `admin_manage__element-user-table-name-${id}` }
-//             >
-//               { name }
-//             </h3>
-//             <h3
-//               data-testid={ `admin_manage__element-user-table-email-${id}` }
-//             >
-//               { email }
-//             </h3>
-//             <h3
-//               data-testid={ `admin_manage__element-user-table-role-${id}` }
-//             >
-//               { role }
-//             </h3>
-//             <button
-//               onClick={}
-//               type="button"
-//               data-testid={ `admin_manage__element-user-table-remove-${id}` }
-//             >
-//               Excluir
-//             </button>
-//           </li>
-//         </ul>
-//       </div>
-//     ))}
-//     </>
-//   );
-// }
+  const removeUserById = (id) => {
+    dispatch(removeUser(id));
+    deleteUser(id);
+  };
 
-// export default AdminTableManagement;
+  return (
+    <div className="checkout-table">
+      { console.log(users) }
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Tipo</th>
+            <th>Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { users.map(({ id, name, email, role }, index) => (
+            <tr key={ index }>
+              <td
+                data-testid={ `admin_manage__element-user-table-item-number-${id}` }
+              >
+                { index + 1 }
+              </td>
+              <td
+                data-testid={ `admin_manage__element-user-table-name-${id}` }
+              >
+                { name }
+              </td>
+              <td
+                data-testid={ `admin_manage__element-user-table-email-${id}` }
+              >
+                { email }
+              </td>
+              <td
+                data-testid={ `admin_manage__element-user-table-role-${id}` }
+              >
+                { renderRole(role) }
+              </td>
+              <td>
+                <button
+                  type="button"
+                  data-testid={ `admin_manage__element-user-table-remove-${id}` }
+                  onClick={ () => removeUserById(id) }
+                >
+                  Excluir
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default AdminTableManagement;
