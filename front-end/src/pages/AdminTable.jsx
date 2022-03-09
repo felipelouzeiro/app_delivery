823efb96b4820b94df8402a031e31c951ce6638f
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-09 15:42:24 -0300

    Feat: Structuring Page AdminTable and use components

--- front-end/src/pages/AdminTable.jsx
+++ front-end/src/pages/AdminTable.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import AdminTableManagement from '../components/AdminTableManagement';
+import AdminForm from '../components/AdminForm';
+import CustomerNavBar from '../components/CustomerNavbar';
+import { getUsers } from '../api';
+import { saveUsers } from '../redux/slices/userSlice';
 
 function AdminTable() {
-  return <AdminTableManagement />;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getUsers()
+      .then((response) => (dispatch(saveUsers(response.data))));
+  });
+
+  return (
+    <div>
+      <CustomerNavBar />
+      <h1>Cadastrar Novo Usuário</h1>
+      <AdminForm />
+      <AdminTableManagement />
+    </div>
+  );
 }
 
 export default AdminTable;
