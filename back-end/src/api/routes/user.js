10321d0889736bc44e8661bf9182236104597ccc
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-09 09:43:39 -0300

    Feat: create route POST /user

--- back-end/src/api/routes/user.js
+++ back-end/src/api/routes/user.js
@@ -1,7 +1,9 @@
 const userRouter = require('express').Router();
 const { getUserController } = require('../../controllers/user');
+const { createUserController } = require('../../controllers/user');
 const { authentication } = require('../../middlewares/authentication');
 
 userRouter.get('/', authentication, getUserController);
+userRouter.post('/', authentication, createUserController);
 
 module.exports = userRouter;
