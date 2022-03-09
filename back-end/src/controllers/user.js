eaad4f3c42645375fce7c208a0de51865d518dc3
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-09 09:44:28 -0300

    Feat: Create service and Controller of Post User route

--- back-end/src/controllers/user.js
+++ back-end/src/controllers/user.js
@@ -1,5 +1,6 @@
 const { success } = require('../utils/dictionary');
 const { getUserService } = require('../services/user');
+const { createUserService } = require('../services/user');
 
 const getUserController = async (req, res, next) => {
   try {
@@ -11,6 +12,20 @@ const getUserController = async (req, res, next) => {
   }
 };
 
+const createUserController = async (req, res, next) => {
+  try {
+    const newUser = req.body;
+    const { role } = req.user;
+    
+    const users = await createUserService(newUser, role);
+
+    return res.status(success).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getUserController,
+  createUserController,
 };
--- back-end/src/services/user.js
+++ back-end/src/services/user.js
@@ -1,4 +1,8 @@
+const md5 = require('md5');
 const { user } = require('../database/models');
+const { registerSchema } = require('../utils/joiSchemas');
+const errorConstructor = require('../utils/errorConstructor');
+const { badRequest, conflict, unauthorized } = require('../utils/dictionary');
 
 const getUserService = async () => {
   const users = await user.findAll();
@@ -11,6 +15,26 @@ const getUserService = async () => {
   return UserData;
 };
 
+const createUserService = async (userPayload, admRole) => {
+  const { name, email, password, role } = userPayload;
+  const { error } = registerSchema.validate({ name, email, password, role });
+  if (error) throw errorConstructor(badRequest, error.message);
+
+  const emailExist = await user.findOne({ where: { email } });
+  if (emailExist) throw errorConstructor(conflict, 'Email already exist');
+
+  if (admRole !== 'administrator') {
+    throw errorConstructor(unauthorized, 'User is not administrator'); 
+  }
+
+  const hash = md5(password);
+  
+  const { dataValues } = await user.create({ name, email, password: hash, role });
+
+  return dataValues;
+};
+
 module.exports = {
   getUserService,
+  createUserService,
 };
