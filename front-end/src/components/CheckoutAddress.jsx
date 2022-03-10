4319b094b93f6ea7041f0a24e4821d8b59a8f78f
Cassio Pereira <cassiorodp@gmail.com>
2022-03-10 19:12:44 -0300

    refactor: add disable checkout button logic

--- front-end/src/components/CheckoutAddress.jsx
+++ front-end/src/components/CheckoutAddress.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { getUsers, postSales } from '../api';
+import { checkoutSchema } from '../utils/schemas';
 
 function CheckoutAddress() {
   const chosenProduct = useSelector((state) => state.chosenProduct.chosenProducts);
@@ -9,6 +10,7 @@ function CheckoutAddress() {
   const [address, setAdress] = useState('');
   const [number, setNumber] = useState('');
   const [seller, setSeller] = useState(0);
+  const [checkoutButton, setCheckoutButton] = useState(true);
   const history = useHistory();
 
   const saleProducts = () => {
@@ -30,6 +32,14 @@ function CheckoutAddress() {
       });
     return totalPrice;
   };
+
+  useEffect(() => {
+    checkoutSchema.isValid({ address, number })
+      .then((valid) => {
+        if (valid) setCheckoutButton(false);
+        else setCheckoutButton(true);
+      });
+  });
 
   const finishOrder = async (e) => {
     e.preventDefault();
@@ -58,56 +68,60 @@ function CheckoutAddress() {
   }, [filterSellers]);
 
   return (
-    <div className="product-page">
-      <label htmlFor="sellers">
-        P. Vendedora Responsável
-        <select
-          required
-          value={ seller }
-          name="sellers"
-          data-testid="customer_checkout__select-seller"
-          onChange={ (event) => setSeller(event.target.value) }
-        >
-          {
-            filterSellers
-              .map((userSeller, index) => (
-                <option
-                  key={ index }
-                  value={ userSeller.id }
-                >
-                  { userSeller.name }
-                </option>))
-          }
-        </select>
-      </label>
-      <label htmlFor="address">
-        Endereço
-        <input
-          required
-          type="text"
-          name="address"
-          onChange={ (event) => setAdress(event.target.value) }
-          data-testid="customer_checkout__input-address"
-        />
-      </label>
-      <label htmlFor="number">
-        Número
-        <input
-          required
-          type="text"
-          name="number"
-          onChange={ (event) => setNumber(event.target.value) }
-          data-testid="customer_checkout__input-addressNumber"
-        />
-      </label>
+    <section className="checkout-address">
+      <div className="checkout-address-input-container">
+        <label className="checkout-address-input" htmlFor="sellers">
+          P. Vendedora Responsável
+          <select
+            required
+            value={ seller }
+            name="sellers"
+            data-testid="customer_checkout__select-seller"
+            onChange={ (event) => setSeller(event.target.value) }
+          >
+            {
+              filterSellers
+                .map((userSeller, index) => (
+                  <option
+                    key={ index }
+                    value={ userSeller.id }
+                  >
+                    { userSeller.name }
+                  </option>))
+            }
+          </select>
+        </label>
+        <label className="checkout-address-input street-name" htmlFor="address">
+          Endereço
+          <input
+            required
+            type="text"
+            name="address"
+            onChange={ (event) => setAdress(event.target.value) }
+            data-testid="customer_checkout__input-address"
+          />
+        </label>
+        <label className="checkout-address-input house-number" htmlFor="number">
+          Número
+          <input
+            required
+            type="text"
+            name="number"
+            onChange={ (event) => setNumber(event.target.value) }
+            data-testid="customer_checkout__input-addressNumber"
+          />
+        </label>
+      </div>
       <button
+        disabled={ checkoutButton }
+        className="checkout-button"
         type="submit"
         onClick={ finishOrder }
         data-testid="customer_checkout__button-submit-order"
       >
         Finalizar Pedido
       </button>
-    </div>
+    </section>
   );
 }
 
